6e70e4b0e63783161794857025cbf299cebd40b9
vugsk <ganchukovnikita@bk.ru>
2023-11-17 07:48:24 +0700

    good

--- src/Module/CursesCpp/Window.cpp
+++ src/Module/CursesCpp/Window.cpp
@@ -1,9 +1,6 @@
 
 #include "Window"
 
-#include <curses.h>
-#include <utility>
-
 
 template <typename T1, typename T2>
 clg_cursescpp::PairNum<T1> clg_cursescpp::convertStructPairNum(clg_cursescpp::PairNum<T2> data)
@@ -17,37 +14,116 @@ T1 clg_cursescpp::convertTypeData(T2 data)
     return static_cast<T1>(data);
 }
 
+
+
+
 /*
 
     Class Window
 
 */
 
-const clg_cursescpp::PairNum<short> clg_cursescpp::Window::SCREEN_XY
-{ 
-    convertTypeData<short>(getmaxx(stdscr)), 
-    convertTypeData<short>(getmaxy(stdscr))
-};
 
+/*
+    Private
+*/
+
+
+const clg_cursescpp::PairNum<int> clg_cursescpp::Window::NULL_XY = {0, 0};
+
+inline void clg_cursescpp::Window::print(const char *text, va_list args, 
+    clg_cursescpp::PairNum<int> xy)
+{
+    wmove(_win, xy._y, xy._x);
+    vw_printw(_win, text, args);
+    wrefresh(_win);
+}
+
+
+/*
+    Public
+*/
+
+short clg_cursescpp::Window::getX() { return _xy._x; }
+short clg_cursescpp::Window::getY() { return _xy._y; }
+
+int clg_cursescpp::Window::getMaxX() { return getmaxx(_win); }
+int clg_cursescpp::Window::getMaxY() { return getmaxy(_win); }
+
+clg_cursescpp::PairNum<int> clg_cursescpp::Window::getMaxXY()
+{ 
+    return { 
+        getMaxX(), 
+        getMaxY() 
+    };
+}
 
 int clg_cursescpp::Window::getChar() { return wgetch(_win); }
-// TODO erorr
 void clg_cursescpp::Window::box(const unsigned a, const unsigned b) { ::box(_win, a, b); }
 void clg_cursescpp::Window::close() { delwin(_win); }
 void clg_cursescpp::Window::refresh() { wrefresh(_win); }
+
+
+void clg_cursescpp::Window::movePrintWin(clg_cursescpp::PairNum<int> xy, 
+    const char* text, ...)
+{
+    va_list args;
+    va_start(args, text);
+    print(text, args, xy);
+    va_end(args);
+}
+
+void clg_cursescpp::Window::printWin(const char* text, ...)
+{
+    va_list args;   
+    va_start(args, text);
+    print(text, args);
+    va_end(args);
+}
+
+
+template<typename F>
+void clg_cursescpp::Window::textSelection(
+    size_t sizeTextArrayForMenu,
+    std::function<F> updateFunc)
+{
+    for (size_t i = 0; i < sizeTextArrayForMenu; i++) updateFunc();
+}
+
+template<typename F1, typename F2>
+void clg_cursescpp::Window::textSelectionTable(
+    size_t sizeFirstArray, size_t sizeSecondArray,
+    std::function<F1> updateFuncRunInLoopFirstArray,
+    std::function<F2> updateFuncRunInLoopSecondArray)
+{
+    for (size_t i = 0; i < sizeFirstArray; i++)
+    {
+        textSelection(sizeSecondArray, updateFuncRunInLoopSecondArray);
+        updateFuncRunInLoopFirstArray();
+    }
+}
+
 
 clg_cursescpp::Window* clg_cursescpp::Window::createWindow(
     const PairNum<int> xy,
     const PairNum<int> widthAndLength)
 {
-    return new Window(convertStructPairNum<short>(xy),
-        newwin(xy._x, xy._y, widthAndLength._x, widthAndLength._y));
+    return new Window(convertStructPairNum<short>(xy), 
+        newwin(xy._y, xy._x, widthAndLength._y, widthAndLength._x));
 }
 
+
+void clg_cursescpp::Window::textSelectionTable() {}
+void clg_cursescpp::Window::textSelection() {}
 void clg_cursescpp::Window::update() {}
 
 
-clg_cursescpp::Window::Window() {}
+clg_cursescpp::Window::Window() 
+    : _win(stdscr)
+    , _xy(convertStructPairNum<short>(getMaxXY()))
+{
+
+}
 
 clg_cursescpp::Window::Window(const PairNum<short> xy, WINDOW* window)
     : _win(window)
--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,26 @@
 
-#include <curses.h>
-#include <iostream>
-
 #include <Window>
 
-class NewWin : public clg_cursescpp::Window {
-    public:
-        NewWin() {
-            
-        }
 
-        void update() override {
-            std::cout << "dkjfl\n";
-        }
-};
 
 int main()
 {
     initscr();
-    NewWin win;
+    clg_cursescpp::Window win;
     win.box(0, 0);
-    win.refresh();
     win.update();
-
-    std::cout << clg_cursescpp::Window::SCREEN_XY._x << '\n';
-
+    win.movePrintWin({10, 5}, "text");
     win.getChar();
     win.refresh();
 
-    clg_cursescpp::Window* test = new NewWin();
-
-    test = win.createWindow({50, 10}, {
-        (clg_cursescpp::Window::SCREEN_XY._x/2)-25, 
-        (clg_cursescpp::Window::SCREEN_XY._y/2)-5
-    });
+    clg_cursescpp::Window* test = win.createWindow(
+        {50, 10},
+        {(win.getX()/2)-25, (win.getY()/2)-5}
+    );
 
     test->box(0, 0);
-    test->refresh();
     test->update();
     test->getChar();
-    test->close();
-
-    win.close();
 
     return 0;
 }

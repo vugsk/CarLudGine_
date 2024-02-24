cf7a16fb456edf7e4ad1aadadfc224e3f56d4109
vugsk <ganchukovnikita@bk.ru>
2024-02-24 18:08:39 +0700

    рабочая версия

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,13 @@
 
 typedef std::string string;
 
-class Error_ : public std::exception
+class Error_ final : public std::exception
 {
 public:
     explicit Error_(string message)
         : message(std::move(message)) {}
 
-    [[nodiscard]] const char *what() const noexcept override
+    [[nodiscard]] const char* what() const noexcept override
     {
         return message.c_str();
     }
@@ -25,7 +25,7 @@ private:
 
 typedef nlohmann::json json;
 
-static json RetrievesDataFromFile_json(const char *name)
+static json RetrievesDataFromFile(const char* name)
 {
     std::ifstream file(name);
     json          data = json::parse(file);
@@ -40,15 +40,15 @@ public:
     ParserJson()
         : m_isMethodBeenRunLeastOnce(false) {}
 
-    ParserJson(const ParserJson &other)                = delete;
-    ParserJson(ParserJson &&other) noexcept            = delete;
-    ParserJson &operator=(const ParserJson &other)     = delete;
-    ParserJson &operator=(ParserJson &&other) noexcept = delete;
+    ParserJson(const ParserJson& other)                = delete;
+    ParserJson(ParserJson&& other) noexcept            = delete;
+    ParserJson& operator=(const ParserJson& other)     = delete;
+    ParserJson& operator=(ParserJson&& other) noexcept = delete;
     ~ParserJson()                                      = default;
 
     template<typename T1>
-    [[nodiscard]] T1 readValueByKey_DataFile(const char *key,
-                                             const json &data)
+    [[nodiscard]] T1 readValueByKeyFromFile(const char* key,
+                                            const json& data)
     {
         if (!m_isMethodBeenRunLeastOnce)
             m_stackData.push(data);
@@ -61,12 +61,12 @@ public:
     }
 
     template<typename T1>
-    void test_write(const char *key, const T1 &data)
+    void test_write(const char* key, const T1& data)
     {
         add(m_dataWriteInFile, key, data);
     }
 
-    void test_close(const char *name)
+    void test_close(const char* name)
     {
         if (!exists(std::filesystem::path{name}) ||
             is_empty(std::filesystem::path{name}))
@@ -75,7 +75,7 @@ public:
         }
         else
         {
-            add(RetrievesDataFromFile_json(name), m_dataWriteInFile);
+            add(RetrievesDataFromFile(name), m_dataWriteInFile);
             writeInFile(name);
         }
         m_dataWriteInFile.clear();
@@ -83,15 +83,15 @@ public:
 
 protected:
     template<typename T1>
-    [[nodiscard]] constexpr bool isDataAnObject(const char *key,
-                                                const auto &data,
-                                                T1 &        retun_value)
+    [[nodiscard]] constexpr bool isDataAnObject(const char* key,
+                                                const auto& data,
+                                                T1&         retun_value)
     {
         if (data.value().is_object())
         {
             m_stackKey.push(data.key().c_str());
             m_isMethodBeenRunLeastOnce = false;
-            retun_value = readValueByKey_DataFile<T1>(key, data.value());
+            retun_value = readValueByKeyFromFile<T1>(key, data.value());
             return false;
         }
         return true;
@@ -99,9 +99,9 @@ protected:
 
     template<typename T1>
     [[nodiscard]] constexpr bool isSuchKeyExist(
-        const char *key, const json &data, T1 &return_value)
+        const char* key, const json& data, T1& return_value)
     {
-        for (auto &i : data.items())
+        for (auto& i : data.items())
         {
             if (i.key() == key)
                 break;
@@ -112,14 +112,16 @@ protected:
             }
             if (const bool isDataObject =
                     isDataAnObject(key, i, return_value); !isDataObject)
+            {
                 return isDataObject;
+            }
         }
         return true;
     }
 
     template<typename T1>
     [[nodiscard]] constexpr bool isThereObjectTheStackAndIsEmpty(
-        const char *key, T1 &return_value)
+        const char* key, T1& return_value)
     {
         if (m_stackData.top().is_object() && m_stackData.top().empty())
         {
@@ -127,26 +129,27 @@ protected:
             m_stackData.top().erase(m_stackKey.top());
             m_stackKey.pop();
             m_isMethodBeenRunLeastOnce = true;
-            return_value               = readValueByKey_DataFile<T1>(key,
-                                                       m_stackData.top());
+            return_value               = readValueByKeyFromFile<T1>(key,
+                                                                     m_stackData
+                                                                      .top());
             return true;
         }
         return false;
     }
 
     template<typename T1>
-    static void add(json &data, const char *key, const T1 &value)
+    static void add(json& data, const char* key, const T1& value)
     {
         data[key] = value;
     }
 
-    static void add(const json &whereFromData, json &whereData)
+    static void add(const json& whereFromData, json& whereData)
     {
-        for (const auto &i: whereFromData.items())
+        for (const auto& i : whereFromData.items())
             add(whereData, i.key().c_str(), i.value());
     }
 
-    void writeInFile(const char *name) const
+    void writeInFile(const char* name) const
     {
         std::ofstream file(name);
         file << m_dataWriteInFile.dump(4);
@@ -165,12 +168,14 @@ class Parser
 {
 public:
     Parser()                        = default;
-    Parser(const Parser &other)     = delete;
-    Parser(Parser &&other) noexcept = delete;
+    // Parser(const Parser& other)     = delete;
+    // Parser(Parser&& other) noexcept = delete;
     ~Parser()                       = default;
 
+    string name;
+
     template<typename T>
-    [[nodiscard]] T read(const string &name, const string &key)
+    [[nodiscard]] T read(const string& name, const string& key)
     {
         formatFile(name);
         existFile(name);
@@ -178,8 +183,8 @@ public:
 
         if (findFormatFile(name) == m_formatFiles[0])
         {
-            return parser_json.readValueByKey_DataFile<T>(key.c_str(),
-                RetrievesDataFromFile_json(name.c_str()));
+            return parser_json.readValueByKeyFromFile<T>(key.c_str(),
+                RetrievesDataFromFile(name.c_str()));
         }
         else if (findFormatFile(name) == m_formatFiles[1])
         {
@@ -191,12 +196,12 @@ public:
     }
 
     template<typename T>
-    void write(const string &key, const T &data)
+    void write(const string& key, const T& data)
     {
         parser_json.test_write(key.c_str(), data);
     }
 
-    void close(const string &name)
+    void close(const string& name)
     {
         if (m_formatFiles[0] == findFormatFile(name))
             parser_json.test_close(name.c_str());
@@ -204,13 +209,13 @@ public:
 
 protected:
     //! rename
-    static void doWork(const char *err_text)
+    static void doWork(const char* err_text)
     {
         try
         {
             throw Error_{err_text};
         }
-        catch (const std::exception &err)
+        catch (const std::exception& err)
         {
             printf("%s", err.what());
         }
@@ -219,65 +224,80 @@ protected:
         // exit(EXIT_FAILURE);
     }
 
-    static void formatFile(const string &name)
+    //! rename
+    static void formatFile(const string& name)
     {
-        for (auto count = 0; const auto &i : m_formatFiles)
+        for (auto count = 0; const auto& i : m_formatFiles)
         {
             if (!name.ends_with(i) && count++ == m_formatFiles.size() - 1)
                 doWork("Error: not format file!!!");
         }
     }
 
-    static void existFile(const string &name)
+    //! rename
+    static void existFile(const string& name)
     {
         if (!exists(std::filesystem::path{name}))
             doWork("Error: not exist file!!!");
     }
 
-    static void emptyFile(const string &name)
+    //! rename
+    static void emptyFile(const string& name)
     {
         if (is_empty(std::filesystem::path{name}))
             doWork("Error: empty file!!!");
     }
 
-    static string findFormatFile(const string &name)
+    //! rename
+    static string findFormatFile(const string& name)
     {
         return name.substr(name.find_last_of('.') + 1);
     }
 
-    static void addListFormatFiles(const string &format)
+    //! rename
+    static void addListFormatFiles(const string& format)
     {
         m_formatFiles.push_back(format);
     }
 
-    static void removeListFormatFiles(const string &format)
+    //! rename
+    static void removeListFormatFiles(const string& format)
     {
         if (format == m_formatFiles[0] || format == m_formatFiles[1])
             doWork(("Error: not delete " + format).c_str());
-        m_formatFiles.erase(std::ranges::find(
-                                              m_formatFiles.begin(),
-                                              m_formatFiles.end(),
-                                              format));
+        m_formatFiles.erase(std::ranges::find(m_formatFiles, format));
     }
 
-    static const string &getListFormatFiles(const size_t index)
+    //! rename
+    static const string& getListFormatFiles(const size_t index)
     {
         return m_formatFiles[index];
     }
 
+    // template<typename T>
+    // static void addParser(const T& parser)
+    // {
+    //     m_parsers<T>.push_back(parser);
+    // }
+    //
+    // template<typename T>
+    // static void removeParser(const int index)
+    // {
+    //     m_parsers<T>.erase(m_parsers<T>.begin() + index);
+    // }
+
 private:
     static std::vector<string> m_formatFiles;
     static ParserJson          parser_json;
-    template<typename T>
-    static std::vector<T> parser;
 };
 
 std::vector<string> Parser::m_formatFiles{"json", "ini"};
 
 ParserJson Parser::parser_json;
 
-template<typename T>
-std::vector<T> parser;
+
+
+#define ui(obj) (#obj)
 
 int main()
 {
@@ -296,6 +316,7 @@ int main()
     p.write("kol", 89);
     p.write("hj", "gyhd"); //! not write -> fix
     p.close("test.json");
+
 
     // clg_parserfilescpp::ParserJsonFiles pjf;
     // std::cout << pjf.read<int>(fileJson.c_str(), "hj") << '\n';
